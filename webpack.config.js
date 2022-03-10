07c761c023e28eb69e919cd20eafaf859ec56728
Raj Shekhar <raj@Rajs-MacBook-Pro.local>
2022-03-11 00:44:25 +0530

    asset management - css and images

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,21 @@ const path = require("path");
 module.exports = {
     entry: "./src/index.js",
     output: {
-        filename: "main.js",
-        path: path.resolve(__dirname, "dist")
+        filename: "bundle.js",
+        path: path.resolve(__dirname, "dist"),
+    },
+    module: {
+        rules: [
+            {
+                //for .css files
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'] //the order of the loaders is crucial
+            },
+            {
+                //for images
+                test: /\.(png|jpg|jpeg|svg|gif)$/i,
+                type: 'asset/resource' //built-in support
+            }
+        ]
     }
 }
